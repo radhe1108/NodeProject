ce1dd419e4b3e1f8b38731cb79d19f965f2aa099
Radhe Shyam <radhe.shyam@nagarro.com>
2019-04-12 17:29:51 +0530

    second commit
    Signed-off-by: Radhe Shyam <radhe.shyam@nagarro.com>

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express()
 const path = require('path')
 const { db } = require('./db')
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use('/',express.static(path.join(__dirname,'public')))
-app.use('/api',require('./routes/api').route)
+app.use(express.urlencoded({ extended: true }))
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/api', require('./routes/api').route)
+
+const port = process.env.PORT || 7676
 db.sync()
   .then(() => {
-    app.listen(7676)
+    app.listen(port)
   })
